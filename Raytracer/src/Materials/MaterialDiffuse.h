79aa72cba76c16b99042a9d4cec4d90c55ef3a22
agarzonp <agarzonp@gmail.com>
2017-05-15 18:19:52 +0200

    Make diffuse material to scatter the ray in a random position from the hit point

--- Raytracer/src/Materials/MaterialDiffuse.h
+++ Raytracer/src/Materials/MaterialDiffuse.h
@@ -1,6 +1,7 @@
 #ifndef MATERIAL_DIFFUSE
 #define MATERIAL_DIFFUSE
 
+#include <random>
 #include "Material.h"
 
 class MaterialDiffuse : public Material
@@ -17,8 +18,22 @@ public:
   // scatter ray
   bool ScatterRay(const Geom3D::RaycastHit& hitInfo, glm::vec3& attenuationOut, Geom3D::Ray& rayOut) const override
   {
-    glm::vec3 offset(0.0f, 0.0f, 0.0f); // TO-DO: add some random offset between [-1.0f, 1.0f] in the unit sphere
-    glm::vec3 target = hitInfo.hitPos + hitInfo.hitNormal + offset;
+		static std::random_device randomDevice;
+		static std::mt19937 randomEngine(randomDevice());
+		static std::uniform_real_distribution<float> distribution(-1.0f, 1.0f);
+
+		// calculate scattered ray direction by getting a random point in the unit sphere
+		glm::vec3 randomPointInUnitSphere;
+		do
+		{
+			randomPointInUnitSphere.x = distribution(randomEngine);
+			randomPointInUnitSphere.y = distribution(randomEngine);
+			randomPointInUnitSphere.z = distribution(randomEngine);
+
+		} while (randomPointInUnitSphere.length() <= 1.0f);
+    
+		glm::vec3 unitSphereCenter = hitInfo.hitPos + hitInfo.hitNormal;
+		glm::vec3 target = unitSphereCenter + randomPointInUnitSphere;
 
     // set scattered ray and attenuation
     rayOut.Origin() = hitInfo.hitPos;
